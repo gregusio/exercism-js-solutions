b5b7442f8633ac7f029394b7b7f17701c2db1ff1
gregusio <grzegorzdynaq@gmail.com>
2023-07-20 23:31:42 +0200

    Solve exercise

--- javascript/factory-sensors/factory-sensors.js
+++ javascript/factory-sensors/factory-sensors.js
@@ -16,7 +16,8 @@ export class OverheatingError extends Error {
  * @throws {Error}
  */
 export function checkHumidityLevel(humidityPercentage) {
-  throw new Error('Implement the checkHumidity function');
+  if (humidityPercentage > 70)
+    throw new Error('too high!!!');
 }
 
 /**
@@ -26,7 +27,11 @@ export function checkHumidityLevel(humidityPercentage) {
  * @throws {ArgumentError|OverheatingError}
  */
 export function reportOverheating(temperature) {
-  throw new Error('Implement the reportOverheating function');
+  if (String(temperature) === 'null')
+    throw new ArgumentError('agrument is null!!!');
+
+  if (temperature > 500)
+    throw new OverheatingError(temperature);
 }
 
 /**
@@ -41,5 +46,17 @@ export function reportOverheating(temperature) {
  * @throws {ArgumentError|OverheatingError|Error}
  */
 export function monitorTheMachine(actions) {
-  throw new Error('Implement the monitorTheMachine function');
+  try {
+    actions.check();
+  } catch (error) {
+    if (error instanceof ArgumentError)
+      actions.alertDeadSensor();
+    else if (error instanceof OverheatingError) {
+      if (error.temperature < 600)
+        actions.alertOverheating();
+      else
+        actions.shutdown();
+    } else
+      throw error;
+  }
 }
