adfa6a446f42bb44e3f7ab5032bbb7aaff4a4b71
gregusio <grzegorzdynaq@gmail.com>
2023-07-22 23:09:33 +0200

    Solve exercise

--- javascript/elyses-looping-enchantments/enchantments.js
+++ javascript/elyses-looping-enchantments/enchantments.js
@@ -9,7 +9,14 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  throw new Error('Implement the cardTypeCheck function');
+  let count = 0;
+  stack.forEach(
+    (number) => {
+      if (number === card)
+        count += 1;
+    }
+  );
+  return count;
 }
 
 /**
@@ -20,5 +27,17 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  throw new Error('Implement the determineOddEvenCards function');
+  let count = 0;
+  if (type) {
+    for (const num of stack) {
+      if (num % 2 === 0)
+        count += 1;
+    }
+  } else {
+    for (const num of stack) {
+      if (num % 2 === 1)
+        count += 1;
+    }
+  }
+  return count;
 }
