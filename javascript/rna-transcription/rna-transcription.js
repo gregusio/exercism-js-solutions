76726f4c9789d15122f6a979cd08469ea729b743
gregusio <grzegorzdynaq@gmail.com>
2023-08-29 02:10:07 +0200

    Solve exercise

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -3,6 +3,26 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRna = () => {
-  throw new Error('Remove this statement and implement this function');
+export const toRna = (dna) => {
+  let rna = "";
+
+  while (dna.length > 0) {
+    switch (dna.at(0)) {
+      case 'G':
+        rna = rna.concat('C');
+        break;
+      case 'C':
+        rna = rna.concat('G');
+        break;
+      case 'T':
+        rna = rna.concat('A');
+        break;
+      case 'A':
+        rna = rna.concat('U');
+        break;
+    }
+    dna = dna.slice(1);
+  }
+
+  return rna;
 };
